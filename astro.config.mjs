43f00816885cf541d374d21e190709a8654bcd71
VojtechSin <vojtech.sin@peveko.cz>
2026-05-17 14:21:19 +0200

    Started Getting started category

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightSidebarTopics from "starlight-sidebar-topics";
+import rehypeExternalLinks from 'rehype-external-links';
 
-// https://astro.build/config
+
+const prod = process.env.NODE_ENV === "production";
+
 export default defineConfig({
+    site: prod ? "https://docs.justsgmod.eu" : undefined,
+    markdown: {
+        rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: [] }]],
+    },
     integrations: [
         starlight({
+            favicon: '/src/assets/favicon.png',
             title: 'Tau\'ri Dev Docs',
+            logo: {
+                light: '/src/assets/tauri_logo_dark.webp',
+                dark: '/src/assets/tauri_logo.webp',
+                replacesTitle: false,
+            },
             social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
             defaultLocale: 'root',
             locales: {
@@ -37,13 +50,23 @@ export default defineConfig({
                             link: '/jsg/',
                             icon: "chevron",
                             items: [
+                                "jsg",
                                 {
                                     label: '1.20.x',
                                     items: [
                                         {
                                             label: 'Getting started',
+                                            items: [{ autogenerate: { directory: 'jsg/1-20-x/getting-started' } }],
+                                        }
+                                    ],
+                                },
+                                {
+                                    label: '1.12.2',
+                                    items: [
+                                        {
+                                            label: 'Getting started',
                                             items: [
-                                                "jsg/1-20-x/getting-started/instalation"
+                                                "jsg/1-12-2/getting-started/installation"
                                             ],
                                         }
                                     ],
